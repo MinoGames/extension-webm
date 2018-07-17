9e25b7d96fededc25adbb22916b7f4a1e280a47c
Jean-Denis Boivin <jeandenis.boivin@gmail.com>
2018-07-17 16:00:18 -0400

    Android build

--- build.sh
+++ build.sh
@@ -3,3 +3,5 @@
 (cd `dirname $0` && lime rebuild . ios  $@)
 (cd `dirname $0` && lime rebuild . mac -debug $@)
 (cd `dirname $0` && lime rebuild . mac  $@)
+(cd `dirname $0` && lime rebuild . android -debug $@)
+(cd `dirname $0` && lime rebuild . android  $@)
